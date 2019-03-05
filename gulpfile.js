94cf5fee8cf389a0504a60e1e5e5445a40dc97cc
Nikolay <laleniko@gmail.com>
2019-03-05 23:34:00 +0300

    add concat uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,7 @@
 const gulp = require('gulp');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+
 gulp.task('time', () => {
 	let today = new Date(); 
 	console.log(today);
@@ -6,10 +9,14 @@ gulp.task('time', () => {
 
 gulp.task('jsMove', () => {
 	return gulp.src('*.js')
+		.pipe(concat('index.js'))
+		.pipe(uglify())
 		.pipe(gulp.dest('script'));
 });
 
 gulp.task('cssMove', () => {
 	return gulp.src('*.css')
+		.pipe(concat('style.css'))
+		.pipe(uglify())
 		.pipe(gulp.dest('style'));
 })
