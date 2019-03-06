5073661d96ad010ac56b19adc6524741c2083670
Nikolay <laleniko@gmail.com>
2019-03-06 21:15:42 +0300

    add source map

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const babel = require('gulp-babel');
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
 const cssnano = require('gulp-cssnano');
+const sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('time', () => {
 	let today = new Date(); 
@@ -11,22 +12,23 @@ gulp.task('time', () => {
 
 gulp.task('jsMove', () => {
 	return gulp.src('*.js')
-		.pipe(babel({
-            presets: ['@babel/env']
-        }))
-		.pipe(concat('index.js'))
-		.pipe(uglify())
+		.pipe(sourcemaps.init())
+			.pipe(concat('index.js'))
+			.pipe(babel({
+	            presets: ['@babel/env']
+	        }))
+			.pipe(uglify())
+		.pipe(sourcemaps.write())
 		.pipe(gulp.dest('script'));
 });
 
 gulp.task('cssMove', () => {
 	return gulp.src('*.css')
-		.pipe(babel({
-            presets: ['@babel/env']
-        }))
-        .pipe(concat('style.css'))
-        .pipe(cssnano())
+		.pipe(sourcemaps.init())
+	        .pipe(concat('style.css'))
+	        .pipe(cssnano())
+	    .pipe(sourcemaps.write())
 		.pipe(gulp.dest('style'));
 });
 
-gulp.task('build', ['jsMove', 'cssMove']);
+gulp.task('default', ['jsMove', 'cssMove']);
